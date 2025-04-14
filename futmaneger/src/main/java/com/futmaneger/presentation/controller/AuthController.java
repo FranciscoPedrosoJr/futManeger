488126d20d33d39aba5f7cd02374e1fd32781260
@francisco.junior10 <francisco.junior@jazztech.com.br>
2025-04-14 17:38:10 -0300

    Retornando o token na rota de login

--- futmaneger/src/main/java/com/futmaneger/presentation/controller/AuthController.java
+++ futmaneger/src/main/java/com/futmaneger/presentation/controller/AuthController.java
@@ -4,8 +4,10 @@ import com.futmaneger.application.dto.TecnicoDto;
 import com.futmaneger.application.usecase.auth.AutenticarTecnicoUseCase;
 import com.futmaneger.application.usecase.tecnico.CadastrarTecnicoUseCase;
 import com.futmaneger.domain.entity.Tecnico;
+import com.futmaneger.infrastructure.security.JwtService;
 import com.futmaneger.presentation.request.CadastrarTecnicoRequest;
 import com.futmaneger.presentation.request.LoginRequest;
+import com.futmaneger.presentation.response.LoginResponse;
 import com.futmaneger.presentation.response.TecnicoResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,9 +39,12 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<TecnicoResponse> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         return autenticarTecnicoUseCase.autenticar(request.email(), request.senha())
-                .map(TecnicoResponse::new)
+                .map(tecnico -> {
+                    String token = JwtService.gerarTokenTecnico(tecnico);
+                    return new LoginResponse(tecnico.getId(), tecnico.getNome(), tecnico.getEmail(), token);
+                })
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.status(401).build());
     }
---
+++ futmaneger/src/main/java/com/futmaneger/presentation/response/LoginResponse.java
@@ -0,0 +1,4 @@
+package com.futmaneger.presentation.response;
+
+public record LoginResponse(Long id, String nome, String email, String token) {
+}
