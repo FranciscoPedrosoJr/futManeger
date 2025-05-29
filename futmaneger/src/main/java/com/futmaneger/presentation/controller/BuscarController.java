a931e3158a514966388808378fa56a4b27412384
@francisco.junior10 <francisco.junior@jazztech.com.br>
2025-05-29 15:25:30 -0300

    Finalizando o crud dos tecnicos

--- futmaneger/src/main/java/com/futmaneger/presentation/controller/BuscarController.java
+++ futmaneger/src/main/java/com/futmaneger/presentation/controller/BuscarController.java
@@ -1,7 +1,9 @@
 package com.futmaneger.presentation.controller;
 
 import com.futmaneger.application.usecase.clube.BuscarClubesUseCase;
+import com.futmaneger.application.usecase.tecnico.BuscarTecnicoUseCase;
 import com.futmaneger.domain.entity.Clube;
+import com.futmaneger.infrastructure.persistence.entity.TecnicoEntity;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class BuscarController {
     private final BuscarClubesUseCase buscarClubesUseCase;
 
-    public BuscarController(BuscarClubesUseCase buscarClubesUseCase) {
+    private final BuscarTecnicoUseCase buscarTecnicoUseCase;
+
+    public BuscarController(BuscarClubesUseCase buscarClubesUseCase, BuscarTecnicoUseCase buscarTecnicoUseCase) {
         this.buscarClubesUseCase = buscarClubesUseCase;
+        this.buscarTecnicoUseCase = buscarTecnicoUseCase;
     }
 
     @GetMapping("/clubes")
     public ResponseEntity<List<Clube>> listarClubes() {
         return ResponseEntity.ok(buscarClubesUseCase.buscarTodos());
+    }
+
+    @GetMapping("/tecnicos")
+    public ResponseEntity<List<TecnicoEntity>> listarTecnicos() {
+        return ResponseEntity.ok(buscarTecnicoUseCase.buscarTodos());
     }
 }
--- futmaneger/src/main/java/com/futmaneger/presentation/controller/ExcluirController.java
+++ futmaneger/src/main/java/com/futmaneger/presentation/controller/ExcluirController.java
@@ -2,6 +2,7 @@ package com.futmaneger.presentation.controller;
 
 import com.futmaneger.application.dto.MensagemResponseDTO;
 import com.futmaneger.application.usecase.clube.ExcluirClubeUseCase;
+import com.futmaneger.application.usecase.tecnico.ExcluirTecnicoUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExcluirController {
     private final ExcluirClubeUseCase excluirClubeUseCase;
 
-    public ExcluirController(ExcluirClubeUseCase excluirClubeUseCase) {
+    private final ExcluirTecnicoUseCase excluirTecnicoUseCase;
+
+    public ExcluirController(ExcluirClubeUseCase excluirClubeUseCase, ExcluirTecnicoUseCase excluirTecnicoUseCase) {
         this.excluirClubeUseCase = excluirClubeUseCase;
+        this.excluirTecnicoUseCase = excluirTecnicoUseCase;
     }
 
     @DeleteMapping("/clubes/{id}")
     public ResponseEntity<MensagemResponseDTO> excluirClube(@PathVariable Long id) {
         return excluirClubeUseCase.excluirPorId(id)
                 .map(nome -> ResponseEntity.ok(new MensagemResponseDTO("O clube " + nome + " foi excluído com sucesso!")))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/tecnicos/{id}")
+    public ResponseEntity<MensagemResponseDTO> excluirTecnico(@PathVariable Long id) {
+        return excluirTecnicoUseCase.excluirPorId(id)
+                .map(nome -> ResponseEntity.ok(new MensagemResponseDTO("O tecnico " + nome + " foi excluído com sucesso!")))
                 .orElse(ResponseEntity.notFound().build());
     }
 }
