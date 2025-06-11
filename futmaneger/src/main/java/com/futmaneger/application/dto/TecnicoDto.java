7c19a212d5a04b054c68bc75480aed8c638f75f1
Francisco Pedroso Junior <113943080+FranciscoPedrosoJr@users.noreply.github.com>
2025-06-11 17:58:34 -0300

    Fix/mvp 01 (#2)
    * Ajuste no tecnico
    
    * Ajuste na classe do tecnico

--- futmaneger/src/main/java/com/futmaneger/application/dto/TecnicoDto.java
+++ futmaneger/src/main/java/com/futmaneger/application/dto/TecnicoDto.java
@@ -1,25 +1,3 @@
 package com.futmaneger.application.dto;
 
-public class TecnicoDto {
-    private String nome;
-    private String email;
-    private String senha;
-
-    public TecnicoDto(String nome, String email, String senha) {
-        this.nome = nome;
-        this.email = email;
-        this.senha = senha;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-}
+public record TecnicoDto(String nome, String email, String senha) {}
