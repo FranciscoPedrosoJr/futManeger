a21c2bddecca82eab786d8dda805274730376781
@francisco.junior10 <francisco.junior@jazztech.com.br>
2025-12-03 11:54:25 -0300

    feat: .

--- futmaneger/src/main/java/com/futmaneger/application/usecase/campeonato/BuscarTabelaCampeonatoUseCase.java
+++ futmaneger/src/main/java/com/futmaneger/application/usecase/campeonato/BuscarTabelaCampeonatoUseCase.java
@@ -14,7 +14,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BuscarTabelaCampeonatoUseCase {
-
     private final TabelaCampeonatoRepository tabelaRepository;
     private final ClubeJpaRepository clubeRepository;
     private final CampeonatoRepository campeonatoRepository;
