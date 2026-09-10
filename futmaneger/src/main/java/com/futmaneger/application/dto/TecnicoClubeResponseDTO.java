29fb43e45742c0ebfb8c8b08b975e5a449b826ff
francisco.junior <francisco.junior@jazztech.com.br>
2026-09-10 14:49:39 -0300

    feat: Adicionado os campos de Estado, Pais e saldo ao retorno da rota de vinculo tecnico clube para ser usado no front.

--- futmaneger/src/main/java/com/futmaneger/application/dto/TecnicoClubeResponseDTO.java
+++ futmaneger/src/main/java/com/futmaneger/application/dto/TecnicoClubeResponseDTO.java
@@ -1,8 +1,13 @@
 package com.futmaneger.application.dto;
 
+import java.math.BigDecimal;
+
 public record TecnicoClubeResponseDTO(
         Long clubeId,
-        String clubeNome
+        String clubeNome,
+        String clubeEstado,
+        String clubePais,
+        BigDecimal clubeSaldo
 ) {
 }
 
